4d60a515fdbb7cdfe7616d007ecb6ccadf1bcfad
cindy21td <m.cindy.prasetio@gmail.com>
2016-10-16 08:17:58 -0700

    Finished

--- quote_parser.py
+++ quote_parser.py
@@ -5,10 +5,10 @@ with open('quote-revised.txt', 'r') as f:
 	content = f.readlines()
 f.close()
 
-data = {'content':[]}
+data = {'content':{}}
 
 lst_tags = []
-lst_topic = []
+dct_tags = {}
 
 for c in content:
 	if c.strip() == "":
@@ -24,13 +24,24 @@ for c in content:
 	sent = TextBlob(unicode(quote, encoding='utf-8', errors='replace')).sentiment.polarity
 
 	dct = {'quote': quote, 'author': author, 'tags': tags, 'sentiment': sent}
-	data['content'].append(dct)
+	#data['content'].append(dct)
+
+	for t in tags:
+		if t.strip() == "":
+			continue
+		if t not in data['content']:
+			data['content'][t] = []
+		if t not in dct_tags:
+			dct_tags[t] = []
+		if quote not in dct_tags[t]:
+			dct_tags[t].append(quote)
+			data['content'][t].append(dct)
 
 	for t in tags:
 		if t not in lst_tags:
 			lst_tags.append(t)
 
-data['list_tags'] = list(lst_tags)
+data['list_tags'] = lst_tags
 
 with open('quote.json', 'w') as f:
 	f.write(json.dumps(data, ensure_ascii=False))
