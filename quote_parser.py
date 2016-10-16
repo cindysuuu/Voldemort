ea56dfd4ad1929e9e9303a24021041a55fcebf69
cindy21td <m.cindy.prasetio@gmail.com>
2016-10-16 01:10:07 -0700

    Updated JSON

--- quote_parser.py
+++ quote_parser.py
@@ -7,6 +7,9 @@ f.close()
 
 data = {'content':[]}
 
+lst_tags = []
+lst_topic = []
+
 for c in content:
 	if c.strip() == "":
 		continue
@@ -16,11 +19,18 @@ for c in content:
 	author = tokens[1].strip()
 	topic = tokens[2].strip()
 	tags = tokens[3].strip().split(',')
+	tags.append(topic)
+
 	sent = TextBlob(unicode(quote, encoding='utf-8', errors='replace')).sentiment.polarity
 
-	dct = {'quote': quote, 'author': author, 'topic': topic, 'tags': tags, 'sentiment': sent}
-	
+	dct = {'quote': quote, 'author': author, 'tags': tags, 'sentiment': sent}
 	data['content'].append(dct)
+
+	for t in tags:
+		if t not in lst_tags:
+			lst_tags.append(t)
+
+data['list_tags'] = list(lst_tags)
 
 with open('quote.json', 'w') as f:
 	f.write(json.dumps(data, ensure_ascii=False))
